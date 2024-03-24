1f226fce842ce25c7193371bbe9550906cf65240
Caberu <lecaberudo@gmail.com>
2024-03-24 09:24:31 +0100

    Ascend fix

---
+++ Scenes/ArriveAtEdge.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+enum SIDE{
+	PLAYER,
+	INQUISITION
+}
+
+@export var side : SIDE
+
+signal arrive_at_edge
+
+func _process(delta):
+	if (side == SIDE.PLAYER):
+		if (global_position.x > 580):
+			arrive_at_edge.emit()
+	elif (side == SIDE.INQUISITION):
+		if (global_position.x < 60):
+			arrive_at_edge.emit()
+	
