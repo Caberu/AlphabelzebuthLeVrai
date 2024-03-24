ab71ff4b9ce13f524533d6281ff69e50581349d9
Giovanni Karra <karragiovanni@gmail.com>
2024-03-24 05:13:30 +0100

    projectiles

--- Scenes/Bullet.gd
+++ Scenes/Bullet.gd
@@ -6,10 +6,13 @@ var hit_range := 5.0
 var dmg := 1
 
 func _process(delta):
-	var dist = target.global_position.x - global_position.x
-	if abs(dist) > hit_range:
-		var direction := 1.0 if dist > 0 else -1.0
-		global_position.x += direction*speed*delta
+	if target != null:
+		var dist = target.global_position.x - global_position.x
+		if abs(dist) > hit_range:
+			var direction := 1.0 if dist > 0 else -1.0
+			global_position.x += direction*speed*delta
+		else:
+			target.find_child("Damageable").damage(dmg)
+			queue_free()
 	else:
-		target.find_child("Damageable").damage(dmg)
 		queue_free()
